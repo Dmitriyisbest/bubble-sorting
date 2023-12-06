c5f2d0a5322ab8a2468f71885ffc6a9ebfae6a42
Dmitriy Best <153114605+Dmitriyisbest@users.noreply.github.com>
2023-12-06 19:15:12 +0200

    Update description of what is happening.sh

--- bubble/description of what is happening.sh
+++ bubble/description of what is happening.sh
@@ -3,6 +3,16 @@ echo "HELLO"
 source ./state_sync_vars.sh
 source ./peers.sh
 
+
+INTERVAL=100
+LATEST_HEIGHT=$(curl -s "http://58.65.160.225:26687/block" | jq -r .result.block.header.height)
+BLOCK_HEIGHT=$(($LATEST_HEIGHT-$INTERVAL)) 
+TRUST_HASH=$(curl -s "http://58.65.160.225:26687/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
+
+# Print out block and transaction hash from which to sync state.
+echo "trust_height: $BLOCK_HEIGHT"
+echo "trust_hash: $TRUST_HASH"
+
 update_test_genesis '.app_state["gov"]["voting_params"]["voting_period"] = "50s"'
 update_test_genesis '.app_state["mint"]["params"]["mint_denom"]=$DENOM' $DENOM
 
